bb6c402f3397e05cc3997952c2642f549f6352b7
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-06-26 23:10:09 +0800

    Add Matrix Multiply Number

--- MatrixOperation_CSharp/Matrix.cs
+++ MatrixOperation_CSharp/Matrix.cs
@@ -4,29 +4,29 @@ namespace MatrixOperations {
     class Matrix {
         private int row;
         private int column;
-        private float[,] data;
+        private float[, ] data;
 
-        public Matrix(int _row, int _col) {
+        public Matrix (int _row, int _col) {
             row = _row;
             column = _col;
             data = new float[_row, _col];
         }
-        public Matrix(float[,] _mat) {
-            row = _mat.GetLength(0);
-            column = _mat.GetLength(1);
+        public Matrix (float[, ] _mat) {
+            row = _mat.GetLength (0);
+            column = _mat.GetLength (1);
             data = _mat;
         }
 
         public int Row { get => row; set => row = value; }
         public int Column { get => column; set => column = value; }
 
-        public void setValue(int _row, int _col, float _val) {
+        public void setValue (int _row, int _col, float _val) {
             data[_row, _col] = _val;
         }
-        public float getValue(int _row, int _col) {
+        public float getValue (int _row, int _col) {
             return data[_row, _col];
         }
-        public void setValues(float[] _vals) {
+        public void setValues (float[] _vals) {
             data = new float[row, column];
             int counter = 0;
             for (int i = 0; i < row; ++i) {
@@ -37,65 +37,65 @@ namespace MatrixOperations {
         }
 
         // Verification of the sizes of the Matrixs
-        public bool CompareSize(Matrix _mat) {
+        public bool CompareSize (Matrix _mat) {
             if (row == _mat.Row && column == _mat.Column)
                 return true;
             return false;
         }
 
         //Addition of two matrix
-        public static Matrix operator +(Matrix _m1, Matrix _m2) {
-            if (_m1.CompareSize(_m2) == false) {
-                throw new Exception("The Size of Matrix Does Not Match");
+        public static Matrix operator + (Matrix _m1, Matrix _m2) {
+            if (_m1.CompareSize (_m2) == false) {
+                throw new Exception ("The Size of Matrix Does Not Match");
             }
-            Matrix result = new Matrix(_m1.row, _m1.column);
+            Matrix result = new Matrix (_m1.row, _m1.column);
             for (int i = 0; i < _m1.row; i++)
                 for (int j = 0; j < _m1.column; j++)
-                    result.setValue(i, j, _m1.getValue(i, j) + _m2.getValue(i, j));
+                    result.setValue (i, j, _m1.getValue (i, j) + _m2.getValue (i, j));
             return result;
         }
 
         // Substraction of two dataes
-        public static Matrix operator -(Matrix _m1, Matrix _m2) {
-            if (_m1.CompareSize(_m2) == false) {
-                throw new Exception("Matrix Size Does Not Match");
+        public static Matrix operator - (Matrix _m1, Matrix _m2) {
+            if (_m1.CompareSize (_m2) == false) {
+                throw new Exception ("Matrix Size Does Not Match");
             }
-            Matrix result = new Matrix(_m1.row, _m1.column);
+            Matrix result = new Matrix (_m1.row, _m1.column);
             for (int i = 0; i < _m1.row; i++)
                 for (int j = 0; j < _m1.column; j++)
-                    result.setValue(i, j, _m1.getValue(i, j) - _m2.getValue(i, j));
+                    result.setValue (i, j, _m1.getValue (i, j) - _m2.getValue (i, j));
             return result;
         }
 
-        // Get new data which is multipled on the decimal number
-        // public Matrix MulAInteger (int integer) {
-        //     Matrix result = new Matrix (row, column);
-        //     for (int i = 0; i < row; i++)
-        //         for (int j = 0; j < column; j++)
-        //             result.data[i, j] = data[i, j] * integer;
-        //     return result;
-        // }
-
         // Multiplication of the Matrixes
-        public static Matrix operator *(Matrix _m1, Matrix _m2) {
+        public static Matrix operator * (Matrix _m1, Matrix _m2) {
             if (_m1.Column != _m2.Row) {
-                throw new Exception("The Size of Matrix Does Not Match ");
+                throw new Exception ("The Size of Matrix Does Not Match ");
             }
-            Matrix result = new Matrix(_m1.Row, _m2.Column);
+            Matrix result = new Matrix (_m1.Row, _m2.Column);
             for (int i = 0; i < _m1.Row; i++)
                 for (int j = 0; j < _m2.Column; j++)
                     for (int k = 0; k < _m1.Column; k++)
-                        result.setValue(i, j, _m1.getValue(i, j) * _m2.getValue(i, j));
+                        result.setValue (i, j, _m1.getValue (i, j) * _m2.getValue (i, j));
             return result;
         }
 
-        public bool Equals(Matrix _mat) {
-            if (CompareSize(_mat) == false) {
+        public static Matrix operator * (Matrix _mat, float _val) {
+            Matrix result = _mat;
+            for (int i = 0; i < _mat.row; ++i) {
+                for (int j = 0; j < _mat.column; ++j) {
+                    result.setValue (i, j, _mat.getValue (i, j) * _val);
+                }
+            }
+            return result;
+        }
+        public bool Equals (Matrix _mat) {
+            if (CompareSize (_mat) == false) {
                 return false;
             }
             for (int i = 0; i < row; ++i) {
                 for (int j = 0; j < column; ++j) {
-                    if (data[i, j] != _mat.getValue(i, j)) {
+                    if (data[i, j] != _mat.getValue (i, j)) {
                         return false;
                     }
                 }
@@ -103,26 +103,26 @@ namespace MatrixOperations {
             return true;
         }
 
-        public override bool Equals(object _obj) {
-            if(_obj == null || this.GetType()!= _obj.GetType()) {
+        public override bool Equals (object _obj) {
+            if (_obj == null || this.GetType () != _obj.GetType ()) {
                 return false;
             }
-            return Equals((Matrix)_obj);
+            return Equals ((Matrix) _obj);
         }
 
-        public override int GetHashCode() {
+        public override int GetHashCode () {
             String tmp_str = "Matrix";
-            for(int i =0;i < row;++i) {
-                for(int j = 0;j < column;++j) {
-                    tmp_str += data[i, j].ToString();
+            for (int i = 0; i < row; ++i) {
+                for (int j = 0; j < column; ++j) {
+                    tmp_str += data[i, j].ToString ();
                 }
                 tmp_str += "n";
             }
-            return base.GetHashCode() ^ tmp_str.GetHashCode();
+            return base.GetHashCode () ^ tmp_str.GetHashCode ();
         }
         //Verification of the Matrix
-        public static bool operator ==(Matrix m1, Matrix m2) {
-            if (m1.CompareSize(m2) == false) {
+        public static bool operator == (Matrix m1, Matrix m2) {
+            if (m1.CompareSize (m2) == false) {
                 return false;
             }
             for (int i = 0; i < m1.Column; i++)
@@ -131,7 +131,7 @@ namespace MatrixOperations {
                         return false;
             return true;
         }
-        public static bool operator !=(Matrix _m1, Matrix _m2) {
+        public static bool operator != (Matrix _m1, Matrix _m2) {
             if (_m1 == _m2) {
                 return false;
             }
@@ -139,11 +139,11 @@ namespace MatrixOperations {
         }
 
         // Matrix transposition
-        public Matrix Transpose() {
-            Matrix result = new Matrix(column, row);
+        public Matrix Transpose () {
+            Matrix result = new Matrix (column, row);
             for (int i = 0; i < column; i++) {
                 for (int j = 0; j < row; j++) {
-                    result.setValue(i, j, data[j, i]);
+                    result.setValue (i, j, data[j, i]);
                 }
             }
             return result;
--- MatrixOperation_CSharp/Program.cs
+++ MatrixOperation_CSharp/Program.cs
@@ -1,31 +1,40 @@
 ﻿using System;
 
-namespace MatrixOperations{
-    class Program{
-        static void Main(string[] args){
+namespace MatrixOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             Matrix m1 = new Matrix(2, 3);
-            m1.setValues(new float[] {1.1f,2.2f,3f,1f,2f,3f});
+            m1.setValues(new float[] { 1.1f, 2.2f, 3f, 1f, 2f, 3f });
             Matrix m2 = new Matrix(3, 2);
-            m2.setValues(new float[] {1f,2f,1f,2f,1f,2f});
-            Matrix tmp_mat = m1*m2;
-            for(int i = 0;i < 2;++ i){
-                for(int j  =0;j <2 ;++ j){
-                    Console.Write(tmp_mat.getValue(i,j));
+            m2.setValues(new float[] { 1f, 2f, 1f, 2f, 1f, 2f });
+            Matrix tmp_mat = m1 * m2;
+            for (int i = 0; i < 2; ++i)
+            {
+                for (int j = 0; j < 2; ++j)
+                {
+                    Console.Write(tmp_mat.getValue(i, j));
                 }
                 Console.WriteLine();
             }
-            Matrix m3 = new Matrix(new float[2,3] {{0,1,-1},{2,3,3}});
-            tmp_mat = m1+m3;
-            for(int i = 0;i < 2;++ i){
-                for(int j  =0;j <3 ;++ j){
-                    Console.Write(tmp_mat.getValue(i,j));
+            Matrix m3 = new Matrix(new float[2, 3] { { 0, 1, -1 }, { 2, 3, 3 } });
+            tmp_mat = m1 + m3;
+            for (int i = 0; i < 2; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    Console.Write(tmp_mat.getValue(i, j));
                 }
                 Console.WriteLine();
             }
-            tmp_mat = m1-m3;
-            for(int i = 0;i < 2;++ i){
-                for(int j  =0;j <3 ;++ j){
-                    Console.Write(tmp_mat.getValue(i,j));
+            tmp_mat = m1 - m3;
+            for (int i = 0; i < 2; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    Console.Write(tmp_mat.getValue(i, j));
                 }
                 Console.WriteLine();
             }
