db75e5f18480891e1197ce1adfdf3861446071d0
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-07-05 19:29:33 +0800

    Updata Vector3 Imp

--- MatrixOperation_CPP/vector3.cpp
+++ MatrixOperation_CPP/vector3.cpp
@@ -3,12 +3,13 @@
 //
 
 #include "vector3.h"
+#include <cmath>
 
 vector3::vector3() {
 
 }
 
-vector3::vector3(double _n0,double _n1,double _n2) {
+vector3::vector3(double _n0, double _n1, double _n2) {
     this->vec3[0] = _n0;
     this->vec3[1] = _n1;
     this->vec3[2] = _n2;
@@ -22,7 +23,7 @@ double vector3::y() const {
     return this->vec3[1];
 }
 
-double vector3::z() const{
+double vector3::z() const {
     return this->vec3[2];
 }
 
@@ -34,6 +35,75 @@ double vector3::g() const {
     return this->vec3[1];
 }
 
-double vector3::b() const{
+double vector3::b() const {
     return this->vec3[2];
+}
+
+inline const vector3 &vector3::operator+() const {
+    return *this;
+}
+
+inline const vector3 &vector3::operator-() {
+    return *this * -1;
+}
+
+inline double vector3::operator[](int _i) const {
+    return this->vec3[_i];
+}
+
+inline double &vector3::operator[](int _i) {
+    return this->vec3[_i];
+}
+
+inline vector3 &vector3::operator+=(const vector3 &_v) {
+    for (int i = 0; i < 3; ++i) {
+        this->vec3[i] += _v[i];
+    }
+    return *this;
+}
+
+inline vector3 &vector3::operator-=(const vector3 &_v) {
+    for (int i = 0; i < 3; ++i) {
+        this->vec3[i] -= _v[i];
+    }
+    return *this;
+}
+
+inline vector3 &vector3::operator*=(const vector3 &_v) {
+    for (int i = 0; i < 3; ++i) {
+        this->vec3[i] *= _v[i];
+    }
+    return *this;
+}
+
+inline vector3 &vector3::operator*=(const double &_d) {
+    for (double &data:this->vec3) {
+        data *= _d;
+    }
+    return *this;
+}
+
+inline vector3 &vector3::operator/=(const vector3 &_v) {
+    for (int i = 0; i < 3; ++i) {
+        this->vec3[i] /= _v[i];
+    }
+    return *this;
+}
+
+inline vector3 &vector3::operator/=(const double &_d) {
+    for (double &data : this->vec3) {
+        data /= _d;
+    }
+    return *this;
+}
+
+inline double vector3::length() const {
+    return sqrt(this->vec3[0] * this->vec3[0] + this->vec3[1] * this->vec3[1] + this->vec3[2] * this->vec3[2]);
+}
+
+inline const vector3 &vector3::normalize() {
+    for (double &data:this->vec3) {
+        data /= length();
+    }
+    return *this;
 }
--- MatrixOperation_CPP/vector3.h
+++ MatrixOperation_CPP/vector3.h
@@ -6,36 +6,74 @@
 #define MATRIXOPERATION_CPP_VECTOR3_H
 
 
+#include <iosfwd>
+
 class vector3 {
 private:
     double vec3[3];
 public:
     vector3();
-    vector3(double,double,double);
+
+    vector3(double, double, double);
 
     inline double x() const;
+
     inline double y() const;
+
     inline double z() const;
+
     inline double r() const;
+
     inline double g() const;
+
     inline double b() const;
 
-    inline const vector3 &operator + () const;
-    inline const vector3 &operator - () ;
-    inline double operator[] (int _i) const;
-    inline double& operator[] (int _i);
+    inline const vector3 &operator+() const;
 
-    inline vector3 &operator += (const vector3& _v);
-    inline vector3 &operator -= (const vector3& _v);
-    inline vector3 &operator *= (const vector3& _v);
-    inline vector3 &operator *= (const float f);
-    inline vector3 &operator /= (const vector3& _v);
-    inline vector3 &operator /= (const float f);
+    inline const vector3 &operator-();
+
+    inline double operator[](int) const;
+
+    inline double &operator[](int);
+
+    inline vector3 &operator+=(const vector3 &);
+
+    inline vector3 &operator-=(const vector3 &);
+
+    inline vector3 &operator*=(const vector3 &);
+
+    inline vector3 &operator*=(const double &);
+
+    inline vector3 &operator/=(const vector3 &);
+
+    inline vector3 &operator/=(const double &);
 
     inline double length() const;
-    inline void normalize();
-    
+
+    inline const vector3 & normalize();
+
 };
 
+inline std::istream &operator>>(std::istream &, vector3 &);
+
+inline std::ostream &operator<<(std::ostream &, const vector3 &);
+
+inline double dot(const vector3 &, const vector3 &);
+
+inline vector3 cross(const vector3 &, const vector3 &);
+
+inline vector3 operator+(const vector3 &, const vector3 &);
+
+inline vector3 operator-(const vector3 &, const vector3 &);
+
+inline vector3 operator*(const vector3 &, const vector3 &);
+
+inline vector3 operator*(const vector3 &, const double &);
+
+inline vector3 operator/(const vector3 &, const vector3 &);
+
+inline vector3 operator/(const vector3 &
+
+.const double &);
 
 #endif //MATRIXOPERATION_CPP_VECTOR3_H
